0fd259cdf4c0703cb632f51d8be2590837329f74
gnd10137 <154660760+gnd10137@users.noreply.github.com>
2024-12-28 12:45:04 +0900

    2024/12/28 상상

---
+++ 2024-12-28 상상.py
@@ -0,0 +1,10 @@
+##import csv
+##
+##f=open("accident.csv", encoding="euc-kr")
+##data=csv.reader(f)
+##print(next(data))
+##for row in data :
+##    row[2:]=map(int, row[2:])
+##    print(row)
+##f.close()
+
