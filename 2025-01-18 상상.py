6487485f612fc0e5ed8400b541495452993ca4f2
gnd10137 <154660760+gnd10137@users.noreply.github.com>
2025-01-18 12:24:44 +0900

    2025/01/18 상상

---
+++ 2025-01-18 상상.py
@@ -0,0 +1,31 @@
+##import numpy as np
+##
+##a=np.random.random()
+##b=np.random.random(5)
+##c=np.random.randint(1,101)
+##d=np.random.randint(1,101,5)
+##
+##print(a)
+##print(b)
+##print(c)
+##print(d)
+
+##import random
+##a=random.random()
+##b=[]
+##for i in range(5) :
+##    b.append(random.random())
+##c=random.randint(1,100)
+##d=[]
+##for i in range(5) :
+##    d.append(random.randint(1,100))
+##print(a)
+##print(b)
+##print(c)
+##print(d)
+
+import random
+dice=[]
+for i in range(10) :
+    dice.append(random.randint(1,6))
+print(dice)
